1440e068b0b51c4345ae8c5ae9bb148aba3c2480
Vijay Joshi <vijaypay@gmail.com>
2025-03-15 18:08:33 +0530

    Adding mongoose schema validatins.

--- models/note.js
+++ models/note.js
@@ -16,7 +16,11 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 10,
+    required: true,
+  },
   important: Boolean,
 });
 

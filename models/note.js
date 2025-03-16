1f8dbeec321cf625857475c80af062583eda3282
Vijay Joshi <vijaypay@gmail.com>
2025-03-17 02:31:47 +0530

    user-login and jwt authentication.

--- models/note.js
+++ models/note.js
@@ -1,27 +1,16 @@
 const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-mongoose
-  .connect(url)
-
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 10,
     required: true,
+    minlength: 5,
   },
   important: Boolean,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 noteSchema.set("toJSON", {
--- mongo.js
+++ mongo.js
@@ -10,29 +10,29 @@ const password = process.argv[2];
 const url = `mongodb+srv://fullstack:${password}@cluster0.ryakg.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set("strictQuery", false);
-
-mongoose.connect(url);
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-});
-
-const Note = mongoose.model("Note", noteSchema);
-
-const note = new Note({
-  content: "CSS is more easy",
-  important: false,
-});
-
-/*note.save().then((result) => {
-  console.log("note saved!", result);
-  mongoose.connection.close();
-});*/
-
-Note.find({ important: true }).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
   });
-  mongoose.connection.close();
+
+  const Note = mongoose.model("Note", noteSchema);
+
+  /*
+  const note = new Note({
+    content: 'HTML is x',
+    important: true,
+  })
+
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  */
+  Note.find({}).then((result) => {
+    result.forEach((note) => {
+      console.log(note);
+    });
+    mongoose.connection.close();
+  });
 });
